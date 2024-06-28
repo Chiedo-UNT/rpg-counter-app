921ef80179dfec1d09a4e793bd71b3f68c222e3a
Chiedo-UNT <53263585+Chiedo-UNT@users.noreply.github.com>
2024-06-28 13:56:46 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,26 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('rpg-counter-store').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/manifest.json',
-      '/icon.png',
-    ])),
+const CACHE_NAME = 'rpg-counter-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/icon.png',
+  '/icon-512.png',
+  'https://unpkg.com/react@17/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
+  'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
