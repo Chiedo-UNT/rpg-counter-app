a71d69e03604f8a240c7faeabd81fc860aa35413
Chiedo-UNT <53263585+Chiedo-UNT@users.noreply.github.com>
2024-06-28 14:59:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,20 +5,21 @@ const urlsToCache = [
   './app.js',
   './manifest.json',
   './icon.png',
-  './icon-512.png',
   'https://unpkg.com/react@17/umd/react.production.min.js',
   'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
   'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
 ];
 
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
